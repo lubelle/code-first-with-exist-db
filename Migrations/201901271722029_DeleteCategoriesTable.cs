8afea2712211382b6801190f70fe82b01917cb15
lubelle <belleliullh@gmail.com>
2019-01-27 12:30:45 -0500

    preserve data before delete table

---
+++ Migrations/201901271722029_DeleteCategoriesTable.cs
@@ -0,0 +1,38 @@
+namespace CodeFirstExistingDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DeleteCategoriesTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo._Categories",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    Name = c.String(),
+                })
+                .PrimaryKey(t => t.Id);
+            Sql("Insert Into _Categories (Name) Select Name From Categories");
+
+            DropTable("dbo.Categories");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            Sql("Insert Into Categories (Name) Select Name From _Categories");
+
+            DropTable("dbo._Categories");
+        }
+    }
+}
