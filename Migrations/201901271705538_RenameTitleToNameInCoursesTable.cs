80aabc1afd1601f622064d114f54ed51a3c9993a
lubelle <belleliullh@gmail.com>
2019-01-27 12:10:36 -0500

    rename a field

---
+++ Migrations/201901271705538_RenameTitleToNameInCoursesTable.cs
@@ -0,0 +1,22 @@
+namespace CodeFirstExistingDatabase.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RenameTitleToNameInCoursesTable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Courses", "Name", c => c.String(nullable: false));
+            Sql("Update Courses Set Name = Title");
+            DropColumn("dbo.Courses", "Title");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Courses", "Title", c => c.String(nullable: false));
+            Sql("Update Courses Set Title = Name");
+            DropColumn("dbo.Courses", "Name");
+        }
+    }
+}
